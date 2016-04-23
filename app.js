0a1182b380f4949c9fd6a343969cedd9a147832d
AdamJackman <adam.jackman@mail.utoronto.ca>
2016-04-23 02:26:25 -0400

    Created a working session set up. May want to think grab session logic as upsert should cover all cases

--- app.js
+++ app.js
@@ -13,27 +13,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-// set a cookie
-app.use(function (req, res, next) {
-  // check if client sent cookie
-  var cookie = req.cookies.cookieName;
-    
-  if (cookie === undefined) {
-    // No cookie present: set a new cookie
-    var randomNumber=Math.random().toString();
-    console.log(randomNumber);  
-    randomNumber=randomNumber.substring(2,randomNumber.length);
-    console.log(randomNumber);  
-    res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
-    console.log('cookie created successfully');
-  } else {
-    console.log('cookie exists', cookie);
-  } 
-  next();
-});
-
-
-
 var server = app.listen(3000, function(){
 	var host = server.address().address;
 	var port = server.address().port;
@@ -45,14 +24,18 @@ app.get('/', function (req, res) {
 });
 
 
-//register/:uname/pword
-app.get()
+//TODO: register/:uname/pword
+//TODO: logout
 
 /**
  * Check that there is a matching user in the users table.
  * If they exist create a session and pass back the session id.
  */
-app.get('/login/:username/:password', function (req, res) {
+app.get('/login/:username/:password', function (req, res) {    
+    if(checkLogged(req) == true){
+        return sendJSON({"error": "User already logged in"}, res);        
+    }
+    
     var queryString = "select userId from users where username=? and encPass=?";
     var username = req.params.username;
     var password = req.params.password;
@@ -62,7 +45,7 @@ app.get('/login/:username/:password', function (req, res) {
            console.log('Error in prepared statement: ' + err);
         } else {
             if(result.length == 1){ 
-                var ses = grabSession(result[0].userId, res, true);
+                grabSession(result[0].userId, res, true);
             } else {
                 sendJSON({"error": "Login failed: Invalid credentials"}, res);
             }
@@ -70,20 +53,10 @@ app.get('/login/:username/:password', function (req, res) {
     });
 });
 
-/*
-* Initially create a session that can be used after logging in
-*/
-function createSession( userId, res ){
-    var queryString = "insert into sessions(userId) values (?)";
-    var query = db.query( queryString, userId, function(err, result){
-        if(err){
-            console.log('Error in query: ' + err);
-        } else {
-            grabSession( userId, res );
-        }     
-    });
+function checkLogged(req){
+    var cookie = req.cookies.session;
+    return (cookie !== undefined && cookie.sessionId)? true : false;
 }
-
 
 /*
 * Grab a session that has already been created.
@@ -96,16 +69,17 @@ function grabSession( userId, res){
 * if the create flag is true then upon finding no matches a new session will be created.
 */ 
 function grabSession( userId, res, create ){
-    var queryString = "select sessionId from sessions where userId=? and DATE(visited_on) > DATE_SUB(CURDATE(), INTERVAL " + sessionTimeout_ + " MINUTE)";
+    var queryString = "select sessionId from sessions where userId=? and visited_on > timestamp(DATE_SUB(NOW(), INTERVAL " + sessionTimeout_ + " MINUTE))";
     var query = db.query( queryString, userId, function(err, result){
         if (err){
             console.log('Error in query: ' + err);
         } else {
-            if(result.length >= 1){               
+            if(result.length >= 1){
+               res.cookie('session',result[0], { maxAge: 900000, httpOnly: true});
                sendJSON(result[0], res);
             } else {
                if(create){
-                   createSession(userId, res);
+                   upsertSession(userId, res);
                } else {
                    sendJSON({"error": "Session invalid: Login required"}, res);
                }
@@ -113,6 +87,59 @@ function grabSession( userId, res, create ){
         }
     });        
 }
+
+/*
+* Initially create a session or update the session that can be used after logging in
+*/
+function upsertSession(userId, res){
+    var queryString = "select sessionId from sessions where userId = ?";
+    var query = db.query( queryString, userId, function(err, result){
+       if(err){
+           console.log("Error in quert: " + err);
+       } else {
+           if(result.length >= 1){               
+               updateSession( userId, res);
+           } else {
+               createSession( userId, res);
+           }
+       }
+    });
+}
+
+function createSession( userId, res ){
+    var queryString = "insert into sessions(userId) values (?)";
+    var query = db.query( queryString, userId, function(err, result){
+        if(err){
+            console.log('Error in query: ' + err);
+        } else {
+            grabSession( userId, res );
+        }     
+    });
+}
+
+function updateSession( userId, res ){
+    var queryString = "update sessions set visited_on=timestamp(NOW()) where userId=?";
+    var query = db.query( queryString, userId, function(err, result){
+        if(err){
+            console.log('Error in query: ' + err);
+        } else {
+            grabSession( userId, res );
+        }     
+    });
+}
+
+/**
+* If we did not match on a login/register and we do not have a cookie then we should be creating an error at this point.
+*/
+app.use(function (req, res, next) {
+  // check if client sent a cookie
+  var cookie = req.cookies.session;    
+  if (cookie === undefined) {
+    sendJSON({"Error": "No session detected"}, res);
+  }
+  next();
+});
+
 
 
 //properties/
