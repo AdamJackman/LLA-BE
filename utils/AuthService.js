5baa5074ba14d80c4856593ee4be8dc3813e285c
AdamJackman <adam.jackman@clara.com>
2016-08-04 19:55:55 -0700

    Introducing a store to abstract away the db

--- utils/AuthService.js
+++ utils/AuthService.js
@@ -1,7 +1,6 @@
 var db = require('../db/db');
 var ResponseService = require('./ResponseService');
-
-const sessionTimeout_ = 15;
+var AuthStore = require('../db/stores/AuthStore');
 
 var AuthService = {
   /* 
@@ -18,7 +17,6 @@ var AuthService = {
   * if the create flag is true then upon finding no matches a new session will be created.
   */
   grabSession: function(userId, res) {
-    var queryString = "select sessionId from sessions where userId=? and visited_on > timestamp(DATE_SUB(NOW(), INTERVAL " + sessionTimeout_ + " MINUTE))";
     var onSuccess = function(err, result){
       if (err) {
         console.log('Error in query: ' + err);
@@ -31,7 +29,7 @@ var AuthService = {
         }
       }
     }
-    var query = db.query( queryString, userId, onSuccess.bind(this));
+    AuthStore.getForUser(userId, onSuccess.bind(this));
   },
 
   /**
@@ -53,7 +51,6 @@ var AuthService = {
     var registerQuery = db.query( registerQueryString, [firstName, lastName, username, password], onSucess.bind(this));
   },
   createSession : function(userId, res) {
-    var queryString = "insert into sessions(userId) values (?)";
     var onSuccess = function (err, result) {
       if (err) {
         console.log('Error in query: ' + err);
@@ -61,10 +58,9 @@ var AuthService = {
         this.grabSession(userId, res);
       }
     }
-    var query = db.query(queryString, userId, onSuccess.bind(this));
+    AuthStore.addSessionForUser(userId, onSuccess.bind(this));
   },
   updateSession : function(userId, res) {
-    var queryString = "update sessions set visited_on=timestamp(NOW()) where userId=?";
     var onSuccess = function(err, result) {
       if (err) {
         console.log('Error in query: ' + err);
@@ -72,13 +68,12 @@ var AuthService = {
         this.grabSession( userId, res );
       }
     }
-    var query = db.query(queryString, userId, onSuccess.bind(this));
-   },
+    AuthStore.updateSessionForUser(userId, onSucess.bind(this));
+  },
    /*
    * Initially create a session or update the session that can be used after logging in
    */
    upsertSession : function(userId, res) {
-     var queryString = "select sessionId from sessions where userId = ?";
      var onSuccess = function(err, result) {
        if (err) {
          console.log("Error in query: " + err);
@@ -88,9 +83,9 @@ var AuthService = {
          } else {
            this.createSession(userId, res);
          }
+       }
      }
-    }
-    var query = db.query(queryString, userId, onSuccess.bind(this));
-   },
+     AuthStore.getForUser(userId, onSuccess.bind(this));
+   }
 };
 module.exports = AuthService;
